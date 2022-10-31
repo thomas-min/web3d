c02f2779d018b9d5719d3bd4ed3fe13566e3f717
Thomas Min <gyeongmin.min@ssupport.co.kr>
2022-10-31 22:36:14 +0900

    Add more examples to canvas

--- 01_canvas/main.js
+++ 01_canvas/main.js
@@ -1,5 +1,23 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+// Draw rectangle
 ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
+// x: number, y: number, w: number, h: number
+ctx.fillRect(10, 10, 100, 100);
+
+// Draw circle
+ctx.fillStyle = "blue";
+ctx.beginPath();
+// x: number, y: number, radius: number, startAngle: number, endAngle: number,
+ctx.arc(200, 60, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+// Draw triangle
+ctx.fillStyle = "red";
+ctx.beginPath();
+// x: number, y: number
+ctx.moveTo(175, 150);
+ctx.lineTo(200, 175);
+ctx.lineTo(200, 125);
+ctx.fill();
